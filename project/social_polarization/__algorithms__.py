7377967a064ceef73bc64613a4135afa95e6c543
leobouts <leobouts@gmail.com>
2020-12-08 23:55:12 +0200

    changed the algorithms to correspond to the latex paper, adjusted them to avoid writting more code

--- project/social_polarization/__algorithms__.py
+++ project/social_polarization/__algorithms__.py
@@ -1,16 +1,29 @@
-import networkx as nx
 from tqdm import tqdm
-
 from __compute_polarization__ import get_polarization
 from __helpers__ import add_edges_and_count_polarization, get_positive_and_negative_values
 
 
-def greedy(k, graph_in):
+def greedy(k, graph_in, first_k_flag):
+
+    """
+    :param k:
+    :param graph_in:
+    :param first_k_flag:
+    :return:
+    """
+
     graph = graph_in.copy()
     k_items = []
+    nodeDict = dict(graph.nodes(data=True))
+
+    positive_nodes, negative_nodes = get_positive_and_negative_values(nodeDict)
+
+    if first_k_flag:
+        positive_nodes = positive_nodes[:100]
+        negative_nodes = negative_nodes[:100]
 
     for i in range(k):
-        edges, polarization = greedy_batch(k, graph)
+        edges, polarization = greedy_batch(k, graph, positive_nodes, negative_nodes)
 
         edge_1 = edges[0][0][0]
         edge_2 = edges[0][0][1]
@@ -21,19 +34,17 @@ def greedy(k, graph_in):
     return k_items, get_polarization(graph)
 
 
-def greedy_batch(k, graph_in):
+def greedy_batch(k, graph_in, positive_nodes, negative_nodes):
     graph = graph_in.copy()
-    nodeDict = dict(graph.nodes(data=True))
     original_polarization = get_polarization(graph)
     addition_info = {}
-
-    positive_nodes, negative_nodes = get_positive_and_negative_values(nodeDict)
 
     for node_pos in positive_nodes:
         for node_neg in tqdm(negative_nodes):
 
             edge_to_add = (node_pos[0], node_neg[0])
 
+            # skip edge if the edge exists in the original graph
             if graph.has_edge(*edge_to_add):
                 continue
 
@@ -53,87 +64,6 @@ def greedy_batch(k, graph_in):
     return k_items, polarization
 
 
-def skip(k, graph_in):
-    graph = graph_in.copy()
-
-    nodeDict = dict(graph.nodes(data=True))
-    positive_dictionary, negative_dictionary = get_positive_and_negative_values(nodeDict)
-
-    first_pass_polarization = []
-
-    first_of_negative = negative_dictionary[0][0]
-
-    edges_to_add_list = []
-
-    for positive_opinion in positive_dictionary:
-        g_copy = graph.copy()
-        g_copy.add_edges_from([(positive_opinion[0], first_of_negative)])
-        polarization_after_addition = get_polarization(g_copy)
-
-        first_pass_polarization.append(polarization_after_addition)
-
-    for i, node_pos in enumerate(positive_dictionary):
-        for j, node_neg in enumerate(negative_dictionary):
-
-            edge_to_add = (node_pos[0], node_neg[0])
-
-            if graph.has_edge(*edge_to_add):
-                continue
-
-            if i == len(positive_dictionary):
-                polarization = add_edges_and_count_polarization(edges_to_add_list, graph)
-                return edges_to_add_list, polarization
-
-            g_copy = graph.copy()
-            g_copy.add_edges_from([edge_to_add])
-            polarization_after_addition = get_polarization(g_copy)
-
-            if first_pass_polarization[i + 1] < polarization_after_addition:
-                break
-
-            edges_to_add_list.append(edge_to_add)
-            if len(edges_to_add_list) == k:
-                polarization = add_edges_and_count_polarization(edges_to_add_list, graph)
-                return edges_to_add_list, polarization
-
-
-def distance(k, graph_in):
-    graph = graph_in.copy()
-    nodeDict = dict(graph.nodes(data=True))
-    positive_dictionary, negative_dictionary = get_positive_and_negative_values(nodeDict)
-
-    edges_to_add_list = []
-
-    for i, node_pos in enumerate(positive_dictionary):
-        node_pos_value = node_pos[1]
-
-        for j, node_neg in enumerate(negative_dictionary):
-
-            node_neg_value = node_neg[1]
-            edge_to_add = (node_pos[0], node_neg[0])
-
-            if graph.has_edge(*edge_to_add):
-                continue
-
-            if i == len(positive_dictionary):
-                polarization = add_edges_and_count_polarization(edges_to_add_list, graph)
-                return edges_to_add_list, polarization
-
-            g_copy = graph.copy()
-            g_copy.add_edges_from([edge_to_add])
-
-            distance_current = abs(node_pos_value - node_neg_value)
-            distance_to_check = abs(positive_dictionary[i + 1][1] - node_neg_value)
-
-            if distance_to_check > distance_current:
-                break
-
-            edges_to_add_list.append(edge_to_add)
-            if len(edges_to_add_list) == k:
-                polarization = add_edges_and_count_polarization(edges_to_add_list, graph)
-                return edges_to_add_list, polarization
-
-
 def expressed(k, graph_in, mode):
     """
     :param k:
@@ -145,13 +75,14 @@ def expressed(k, graph_in, mode):
     nodeDict = dict(graph.nodes(data=True))
     addition_info = {}
 
-    positive_dictionary, negative_dictionary = get_positive_and_negative_values(nodeDict)
+    positive_nodes, negative_nodes = get_positive_and_negative_values(nodeDict)
 
-    for i, node_pos in enumerate(positive_dictionary):
-        for j, node_neg in enumerate(negative_dictionary):
+    for node_pos in positive_nodes:
+        for node_neg in negative_nodes:
 
             edge_to_add = (node_pos[0], node_neg[0])
 
+            # skip edge if the edge exists in the original graph
             if graph.has_edge(*edge_to_add):
                 continue
 
@@ -180,6 +111,3 @@ def expressed(k, graph_in, mode):
     polarization = add_edges_and_count_polarization(edges_to_add_list, graph)
 
     return k_items, polarization
-
-
-
