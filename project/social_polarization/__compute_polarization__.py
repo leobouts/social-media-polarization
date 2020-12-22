b4b95b06deb15a2134029b09c8f4a8d69497f00d
leobouts <leobouts@gmail.com>
2020-12-22 21:08:20 +0200

    added way to compute the polarization with convergence, this is faster and can be adjusted to be faster according to the accuracy nedded

--- project/social_polarization/__compute_polarization__.py
+++ project/social_polarization/__compute_polarization__.py
@@ -1,6 +1,10 @@
+import time
+
 from scipy.sparse import identity
 import networkx as nx
 import numpy as np
+from numpy.linalg import norm
+from tqdm import trange
 
 
 def get_polarization(g):
@@ -13,6 +17,8 @@ def get_polarization(g):
     :param g: networkx graph with value attributes
     :return: Value of the polarization index
     """
+
+    start = time.time()
 
     no_of_nodes = len(g.nodes)
 
@@ -31,6 +37,48 @@ def get_polarization(g):
     squared = np.square(solutions)
 
     summed = np.sum(squared)
+    end = time.time()
+
+    print("time:", end - start)
 
     # result is normalized according to network size
     return summed / no_of_nodes
+
+
+def friedkinJohnsen(g):
+
+    start = time.time()
+    N = len(g.nodes)
+
+    s = list(nx.get_node_attributes(g, 'value').values())
+
+    new_opinions = [0] * N
+
+    convergence = 1
+
+    while 1:
+
+        for i in range(len(new_opinions)):
+
+            sum_z = new_opinions[i]
+            neighbors = list(g.neighbors(i + 1))
+
+            for neighbor in neighbors:
+                sum_z += new_opinions[neighbor - 1]
+
+            new_opinions[i] = (s[i] + sum_z) / (1 + len(neighbors) + 1)
+
+        # squaring and summing the opinion vector
+        squared = np.square(new_opinions)
+
+        summed = np.sum(squared)
+
+        if abs(convergence - (summed / N)) < 0.001:
+            break
+
+        convergence = summed / N
+    end = time.time()
+
+    print("time:", end - start)
+    # result is normalized according to network size
+    return summed / N
